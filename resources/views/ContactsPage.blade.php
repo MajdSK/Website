d9e5b750b2a20d7c9622c986d0862045baadfe5d
majdSK <majdskikr2005@gmail.com>
2026-03-22 15:33:27 +0300

    ui editing

--- resources/views/ContactsPage.blade.php
+++ resources/views/ContactsPage.blade.php
@@ -1,37 +1,41 @@
 <link href="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.snow.css" rel="stylesheet" />
 <x-layout title="Contact us" stylingsheet="resources/css/Contact.css">
-  <form action="/ContactsPage" method="POST" id="identifier">
-    @csrf
-    <div id="container">
+  <div id="sendingCont">
+    <form action="/ContactsPage" method="POST" id="identifier">
+      @csrf
+      <div id="container">
 
-    </div>
-    <input class="button send" type="submit" value="save" name="text">
-    <input class="button" type="button" value="discard" onclick="discardQuillContent()">
-    <textarea name="text" style="display:none" id="text"></textarea>
-    <script src="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.js"></script>
-    <script>
-      const quill = new Quill('#container', {
-        theme: 'snow',
-        placeholder: 'type your message here...',
-        modules: {
-          toolbar: [
-            ['bold', 'italic', 'underline', 'strike'],
-            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-            [{ 'indent': '-1' }, { 'indent': '+1' }]
-          ]
+      </div>
+      <div id="buttonsCont">
+        <input class="button send" type="submit" value="save" name="text">
+        <input class="button" type="button" value="discard" onclick="discardQuillContent()">
+      </div>
+      <textarea name="text" style="display:none" id="text"></textarea>
+      <script src="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.js"></script>
+      <script>
+        const quill = new Quill('#container', {
+          theme: 'snow',
+          placeholder: 'type your message here...',
+          modules: {
+            toolbar: [
+              ['bold', 'italic', 'underline', 'strike'],
+              [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+              [{ 'indent': '-1' }, { 'indent': '+1' }]
+            ]
+          }
+        });
+        const form = document.getElementById('identifier');
+        form.onsubmit = function () {
+          const text = document.getElementById('text');
+          text.value = quill.root.innerText;
+        };
+        function discardQuillContent() {
+          // Clear the Quill editor
+          quill.setText('');
         }
-      });
-      const form = document.getElementById('identifier');
-      form.onsubmit = function () {
-        const text = document.getElementById('text');
-        text.value = quill.root.innerText;
-      };
-      function discardQuillContent() {
-        // Clear the Quill editor
-        quill.setText('');
-      }
-    </script>
-  </form>
+      </script>
+    </form>
+  </div>
   <div id="sent">
     <form action="/search">
       <div id="searchingCont">
@@ -40,22 +44,20 @@
       </div>
     </form>
     <form action="/delOldMessages" method="GET">
-      <div>
-        <div id="savedMessages">
-          @if (count($text))
-            <h3 style="color: rgb(110,0,0)">your messages: </h3>
-            <ul>
-              @foreach ($text as $message)
-                <li> {{ $message->id }}- {{ $message->MessageBody }} <span
-                    style="color: rgb(110,0,0,0.5); margin-left: 30px;">{{ $message->created_at }}</span></li>
-              @endforeach
-            </ul>
-          @else
-            <h3 style="color: rgb(110,0,0)">No Messages yet...</h3>
-          @endif
-        </div>
-        <input class="delOld button" name="delete" type="submit" value="Delete Old Messages">
+      <div id="savedMessages">
+        @if (count($text))
+          <h3 style="color: rgb(110,0,0)">your messages: </h3>
+          <ul>
+            @foreach ($text as $message)
+              <li> {{ $message->id }}- {{ $message->MessageBody }} <span
+                  style="color: rgb(110,0,0,0.5); margin-left: 30px;">{{ $message->created_at }}</span></li>
+            @endforeach
+          </ul>
+        @else
+          <h3 style="color: rgb(110,0,0)">No Messages yet...</h3>
+        @endif
       </div>
+      <input class="delOld button" name="delete" type="submit" value="Delete Old Messages">
     </form>
   </div>
 </x-layout>
